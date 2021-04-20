7ab0f94da235b9fe21b61f8e9dc15d5bdcf114fa
Matthew789123 <Matthew789123@gmail.com>
2021-04-20 15:24:03 +0200

    Initial commit

---
+++ GradebookV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GradebookV2.MvcApplication" Language="C#" %>
---
+++ GradebookV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GradebookV2.Startup))]
+namespace GradebookV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
