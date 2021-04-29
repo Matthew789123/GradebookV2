a53b25f3462e6dc058a10ed54802d001e7e1fd19
Matthew789123 <Matthew789123@gmail.com>
2021-04-29 14:06:16 +0200

    front page

--- GradebookV2/Controllers/AccountController.cs
+++ GradebookV2/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace GradebookV2.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Potwierdź konto", "Potwierdź konto, klikając <a href=\"" + callbackUrl + "\">tutaj</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "News");
                 }
                 AddErrors(result);
             }
@@ -392,7 +392,7 @@ namespace GradebookV2.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "News");
         }
 
         //
@@ -449,7 +449,7 @@ namespace GradebookV2.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "News");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
