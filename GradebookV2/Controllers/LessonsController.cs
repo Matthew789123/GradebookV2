4fb3752a212bd1f1c709e6fc239cad491d9f8a9d
Matthew789123 <Matthew789123@gmail.com>
2021-05-24 19:07:20 +0200

    adding new lessons

--- GradebookV2/Controllers/LessonsController.cs
+++ GradebookV2/Controllers/LessonsController.cs
@@ -34,7 +34,7 @@ namespace GradebookV2.Controllers
             ViewBag.subject = db.Subjects.First(s => s.SubjectId == subjectId).Name;
             ViewBag.subjectId = subjectId;
             ViewBag.classId = classId;
-            return View("Lessons", db.Lessons.Where(l => l.ClassId == classId && l.SubjectId == subjectId).OrderBy(l => l.Number).ToList());
+            return View("Lessons", db.Lessons.Where(l => l.ClassId == classId && l.SubjectId == subjectId).OrderByDescending(l => l.Number).ToList());
         }
 
         [Authorize(Roles = "Teacher")]
@@ -44,6 +44,29 @@ namespace GradebookV2.Controllers
             lesson.ClassId = classId;
             lesson.SubjectId = subjectId;
             return View("Create", lesson);
+        }
+
+        [Authorize(Roles = "Teacher")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Title,Content,Files,SubjectId,ClassId,Files")] Lesson lesson, HttpPostedFileBase[] files)
+        {
+            string id = User.Identity.GetUserId();
+            if (db.SubjectClassTeacher.Where(sct => sct.ClassId == lesson.ClassId && sct.SubjectId == lesson.SubjectId && sct.TeacherId == id).ToList().Count == 0)
+                return RedirectToAction("Index", "News", null);
+            lesson.Number = db.Lessons.Where(l => l.SubjectId == lesson.SubjectId && l.ClassId == lesson.ClassId).ToList().Count();
+            lesson.Class = db.Classes.First(c => c.ClassId == lesson.ClassId);
+            lesson.Subject = db.Subjects.First(s => s.SubjectId == lesson.SubjectId);
+            foreach (HttpPostedFileBase file in files)
+            {
+                File f = new File();
+            }
+            if (ModelState.IsValid)
+            {
+                db.Lessons.Add(lesson);
+                db.SaveChanges();
+            }
+            return RedirectToAction("getLessons", new { lesson.ClassId, lesson.SubjectId });
         }
     }
 }
--- GradebookV2/Models/Lesson.cs
+++ GradebookV2/Models/Lesson.cs
@@ -13,6 +13,7 @@ namespace GradebookV2.Models
         [Required]
         public string Title { get; set; }
         public string Content { get; set; }
+        [Required]
         public int Number { get; set; }
         [Required]
         public int SubjectId { get; set; }
