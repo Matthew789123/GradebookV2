0376fc67d05777f35202eb186c44a9f49cd8bbd8
miikhaiil <73438524+miikhaiil@users.noreply.github.com>
2021-06-07 14:05:49 +0200

    Messagebox, database changes

---
+++ GradebookV2/Migrations/202106071050308_messagesChangesFix.cs
@@ -0,0 +1,16 @@
+namespace GradebookV2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class messagesChangesFix : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
