a0a0678b8c6ae30d716ec7cb14d2ac25dbf1f238
miikhaiil <73438524+miikhaiil@users.noreply.github.com>
2021-06-08 15:11:37 +0200

    Revert "sending messages V1"
    This reverts commit 448cdeabd9f1c7abf4f972692c4281b10c673538.

--- GradebookV2/Controllers/MessagesController.cs
+++ GradebookV2/Controllers/MessagesController.cs
@@ -18,8 +18,6 @@ namespace GradebookV2.Controllers
             var receivedMessages = db.UserMessages.Where(u => u.ReceiverId == userId).ToList();
             var sendedMessages = db.UserMessages.Where(u => u.SenderId == userId).ToList();
             var model = new MessageBoxViewModel();
-            model.ReceivedMessages = new List<Message>();
-            model.SendedMessages = new List<Message>();
             foreach (UserMessage m in receivedMessages)
             {
                 model.ReceivedMessages.Add(m.Message);
@@ -34,50 +32,6 @@ namespace GradebookV2.Controllers
         public ActionResult NewMessage()
         {
             return View();
-        }
-
-        [HttpPost]
-        public ActionResult NewMessage(string title, string message)
-        {
-            string teacherId = User.Identity.GetUserId();
-            var teacher = db.Users.First(u => u.Id == teacherId);
-            var @class = db.Classes.First(u => u.TeacherId == teacherId);
-            List<ApplicationUser> Receivers = new List<ApplicationUser>();
-            foreach(ApplicationUser s in @class.Students)
-            {
-                if(s.ParentId != null)
-                {
-                    var temp = db.Users.First(u => u.Id == s.ParentId);
-                    Receivers.Add(temp);
-                }
-                
-            }
-
-            Message Message = new Message();
-            Message.Content = message;
-            Message.Title = title;
-            Message.Date = DateTime.Now;
-            db.Messages.Add(Message);
-
-
-            foreach(ApplicationUser r in Receivers)
-            {
-                UserMessage UserMessage = new UserMessage();
-                UserMessage.SenderId = teacherId;
-                UserMessage.Sender = teacher;
-                UserMessage.Message = Message;
-                UserMessage.ReceiverId = r.Id;
-                UserMessage.Receiver = r;
-                db.UserMessages.Add(UserMessage);
-                if (teacher.UserMessages == null)
-                {
-                    teacher.UserMessages = new List<UserMessage>();
-                }
-                teacher.UserMessages.Add(UserMessage);
-            }
-
-            db.SaveChanges();
-            return RedirectToAction("MessageBox");
         }
 
     }
